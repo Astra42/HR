0ec0a8c148d9e99fe988777abab4af76a81d8875
Denis <denis.israfilov2002@mail.ru>
2022-10-06 14:12:54 +0500

    Создал .env для хренения ключа. Немного поправил installed_apps Обновление requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from dotenv import load_dotenv
 
 import os
 
@@ -11,7 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*tjwx3wj*=0lie%=p=n%y3lq^hh!ciglo-(vjzjlq7b5%yn=ho'
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,10 +32,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'frontend.apps',
-    'mailings.apps',
-    'resumes.apps',
-    'vacancies.apps',
+    'frontend.apps.FrontendConfig',
+    'mailings.apps.MailingsConfig',
+    'resumes.apps.ResumesConfig',
+    'vacancies.apps.VacanciesConfig',
 ]
 
 MIDDLEWARE = [
