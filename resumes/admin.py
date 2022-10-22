1f332c56d0879505e1fcb4f9d878e21060e0ab89
Ivan-Dyncan <dodzegun@bk.ru>
2022-10-22 14:51:50 +0500

    Update admin.py

--- resumes/admin.py
+++ resumes/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Resumes)
+class ResumesAdmin(admin.ModelAdmin):
+    list_display = ("creator_id", "created_date", "updated_date", "is_published")
+    search_fields = ("creator_id", "created_date")
+    list_editable = ("is_published",)
+    list_filter = ("is_published","created_date")
+
+
+admin.site.register(Resumes, ResumesAdmin)
