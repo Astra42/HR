97729c6b1b044c733b4df944908ca6e9c17889bc
Ivan-Dyncan <dodzegun@bk.ru>
2022-10-22 14:53:41 +0500

    Update user.py

--- user/models/user.py
+++ user/models/user.py
@@ -9,3 +9,6 @@ class User(User):
     photo = models.ImageField(upload_to="photo/%Y/%m/%d/", verbose_name="Фото")
     roles = models.ManyToManyField(Roles)
     tags = models.ManyToManyField(Tags)
+    
+    class Meta():
+        ordering = ["-date_joined", "username"]
