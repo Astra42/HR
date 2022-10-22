f3a67fb0adc0cbf289be451a51a2c67aeda19f01
Ivan-Dyncan <dodzegun@bk.ru>
2022-10-22 14:56:02 +0500

    Update admin.py

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Vacansies)
+class VacanciesAdmin(admin.ModelAdmin):
+    list_display = ("title", "creator_id", "created_date", "updated_date", "is_published")
+    search_fields = ("title", "creator_id", "created_date")
+    list_editable = ("is_published",)
+    list_filter = ("is_published", "created_date")
+
+
+admin.site.register(Vacansies, VacanciesAdmin)
