3bd3936e634e2347a3ae4bdc7ada6ce45b5bbd32
Astra42 <enshov@mail.ru>
2022-11-07 16:44:29 +0500

    добавил миксин для резюме и вакансии

--- resumes/models.py
+++ resumes/models.py
@@ -7,17 +7,19 @@ from user.models.user import User
 def path_to_doc(instance, file_name):
     return "resumes/{0}/{1}".format(instance.creator_id, file_name)
 
-
-class Resume(models.Model):
+class ResumeMixin(models.Model):
     title = models.CharField(max_length=50, verbose_name="Title")
     is_published = models.BooleanField(default=True, verbose_name="Is published")
-    description = models.TextField(verbose_name="Description",)
-    experience = models.TextField(blank=True, verbose_name="Experience",)
+    description = models.TextField(verbose_name="Description", )
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Created")
     updated_date = models.DateTimeField(auto_now=True, verbose_name="Updated")
     creator_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Creator")
-    doc = models.FileField(verbose_name="doc", upload_to=path_to_doc, blank=True)
     slug = models.SlugField(max_length=55, unique=True, verbose_name="URL", blank=True)
+
+
+class Resume(ResumeMixin, models.Model):
+    experience = models.TextField(blank=True, verbose_name="Experience",)
+    doc = models.FileField(verbose_name="doc", upload_to=path_to_doc, blank=True)
 
     def save(
             self, force_insert=False, force_update=False, using=None, update_fields=None
--- vacancies/models.py
+++ vacancies/models.py
@@ -1,19 +1,11 @@
 from resumes.models import *
 
 
-class Vacancy(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Title")
-    is_published = models.BooleanField(default=True, verbose_name="Is published")
-    description = models.TextField(verbose_name="Description",)
-    qualification = models.TextField(verbose_name="Qualifications",)
+class Vacancy(ResumeMixin, models.Model):
+    qualification = models.TextField(verbose_name="Qualifications", )
     salary_from = models.IntegerField(null=True, verbose_name="Salary from")
     salary_to = models.IntegerField(null=True, verbose_name="Salary to")
-    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Created")
-    updated_date = models.DateTimeField(auto_now=True, verbose_name="Updated")
-    creator_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Creator")
     resumes = models.ManyToManyField(Resume, blank=True)
-    slug = models.SlugField(max_length=55, unique=True, verbose_name="URL", blank=True)
-
 
     def save(
         self, force_insert=False, force_update=False, using=None, update_fields=None
