639abd078955adfd7df3b3f70ebcfd6ea8854729
Denis <denis.israfilov2002@mail.ru>
2022-12-20 16:15:55 +0500

    пофиксил доступ к резюме дитейл

--- resumes/urls.py
+++ resumes/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.ResumeListAPIView.as_view()),
     # Get request user resume
     path('get_resume/', views.GetResumeAPI.as_view(), name='get_resume'),
-    path('<slug:slug>/', views.ResumeDetailAPIView.as_view()),
+    path('<slug:slug>/', views.ResumeDetailAPIView.as_view(), name='resume_detail'),
 ]
