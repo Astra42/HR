076d10dded6678ea082ba7e6e51d7cc5f944c20d
Denis <denis.israfilov2002@mail.ru>
2022-12-20 11:19:25 +0500

    reset password changes

--- user/urls.py
+++ user/urls.py
@@ -6,21 +6,32 @@ from user.views.authentication import *
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='auth_login'),
+    # Update access token
     path('login/refresh/', TokenRefreshView.as_view(), name='auth_refresh'),
+    # Check refresh token verify
     path('verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('logout/', LogoutAPIView.as_view(), name='auth_logout'),
     path('register/', RegisterAPIView.as_view(), name='auth_register'),
     path('email_verify/', VerifyEmailAPIView.as_view(), name='email_verify'),
     path('change_password/', ChangePasswordAPIView.as_view(), name='auth_change_password'),
-    # path('change_email/', ChangeEmailAPIView.as_view(),
-    #     name='auth_change_email'),
     path('my_profile/', MyProfileAPI.as_view(), name='my_profile'),
     path('update_profile/<str:username>/', UpdateProfileView.as_view(), name='update_profile'),
+
+    # Profile by username
     path('profile/<str:username>/', ProfileAPI.as_view(), name='profile'),
+
+    # Password reset
     path('request-reset-email/', RequestPasswordResetEmail.as_view(), name="request-reset-email"),
-    path('password-reset/<uidb64>/<token>/', PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    # path('password-reset/<uidb64>/<token>/', PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
     path('password-reset-complete/', SetNewPasswordAPIView.as_view(), name='password-reset-complete'),
-    path('counties/', CountryList.as_view(), name='country_list'),
+
+    # Phones POST or DELETE
     path('phones/', PhoneAPI.as_view(), name='phones'),
+
+    # Get request user resume
     path('get_resume/', GetResumeAPI.as_view(), name='get_resume'),
+
+    # Country list for form
+    path('counties/', CountryList.as_view(), name='country_list'),
+
 ]
